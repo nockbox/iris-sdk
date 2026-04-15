420f43bd89a51ff54461da2c753e0c9a67096ace
Gohlub <62673775+Gohlub@users.noreply.github.com>
2026-04-14 23:39:51 -0400

    add sign-tx-request guard in the SDK

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export * from './provider.js';
 export * from './migration.js';
 export * from './errors.js';
 export * from './constants.js';
+export * from './validate-sign-tx-request.js';
 export * from './compat.js';
 export * from './bridge.js';
 export * as wasm from './wasm.js';
